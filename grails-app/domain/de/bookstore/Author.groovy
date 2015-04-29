e0f2210338e115365b11a313ffe869386dc0663f
Lars Hueper <lhueper@tecracer.de>
2015-04-29 13:36:59 +0200

    Added nickName

--- grails-app/domain/de/bookstore/Author.groovy
+++ grails-app/domain/de/bookstore/Author.groovy
@@ -4,12 +4,14 @@ class Author {
 
     String name
     Date birthday
+    String nickName
     List<Book> books = []
 
     static hasMany = [books: Book]
 
     static constraints = {
         birthday nullable: true
+        nickName nullable: true
     }
 
 }
