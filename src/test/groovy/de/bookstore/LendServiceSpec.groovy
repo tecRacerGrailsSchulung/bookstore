729f87d0a15a9f32c0f6736c19cc5c91d45d4a86
Jan Mönnich <jan.moennich@gmx.de>
2015-04-29 15:09:48 +0200

    Fixed test

--- src/test/groovy/de/bookstore/LendServiceSpec.groovy
+++ src/test/groovy/de/bookstore/LendServiceSpec.groovy
@@ -30,8 +30,11 @@ class LendServiceSpec extends Specification {
 
         and:
         def customer = new Customer()
-        def formatter = SimpleDateFormat.dateInstance
+        def formatter = SimpleDateFormat.getDateInstance(SimpleDateFormat.DEFAULT, LocaleContextHolder.locale)
         def days = grailsApplication.config.getProperty('de.bookstore.defaultLendDays') as Integer
+
+        and:
+        messageSource.addMessage('lend.success', LocaleContextHolder.locale, 'The book was successfully lended until {0,date}')
 
         when:
         LendService.LendResult result = service.lendBook(testDataService.book1, customer)
@@ -40,7 +43,7 @@ class LendServiceSpec extends Specification {
         result.success
         result.message == 'The book was successfully lended until ' + formatter.format(now + days)
         result.lend.id
-        result.lend.book == book
+        result.lend.book == testDataService.book1
         result.lend.dateFrom == now
         result.lend.dateTo == now + days
         result.lend.customer == customer
